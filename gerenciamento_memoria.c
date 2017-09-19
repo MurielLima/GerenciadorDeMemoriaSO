c02c85cc94e1dcaba8a811a8ed9d113a2494b16c
MurielLima <muriellima2@gmail.com>
2017-09-19 15:12:03 -0300

    arquivo main principal
    First_Fit criado em sala de aula

---
+++ gerenciamento_memoria.c
@@ -0,0 +1,47 @@
+//Disciplina de Algoritmos e Programação
+//Criado por:  Muriel José Lima
+//
+//O programa tem a função de simulador um gerenciador de memória
+//
+//
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#define N 50
+int FirstF(int vetor[N],int alloc){//funçao first fit  localiza o primeiro espaço com o tamanho da alocação e feito
+    int i,pos,fre=0;
+    for(i=1;i<=N;i++)//vasculha o vetor
+        if(vetor[i-1]==0){//i inicia com 1 para o caso do espaço ser 1, ficaria (0-1)=-1 
+            fre++;
+            if(alloc==fre){//o espaço encontrado é o certo
+                pos=i-alloc;
+                return pos;
+            }
+        }else
+                fre=0;
+    return -1;
+}
+int main(){
+    int vetor[N];
+    int i,aloc,a;
+    srand(time(NULL));
+    for(i=0;i<N;i++)
+        vetor[i]=rand() % 2; 
+    do{
+        printf("(");
+        for(i=0;i<N;i++)
+            printf("[%i] ",vetor[i]);
+        printf(")");    
+        printf("\nInforme a quantidade de memoria a ser alocada: ");
+        scanf("%i",&aloc);
+        a=FirstF(vetor,aloc);
+        if(a==-1){
+            printf("Espaço não encontrado.\n");
+        }else{
+            printf("Espaço encontrado com inicio na posição %i!!\n",a);
+
+        }
+        printf("Deseja continuar? (1/0): ");
+        scanf("%i",&i);
+    }while(i!=0);
+}
